3c36ab7e271b257c244f9791adb4e6c0f12144d7
SiddhantJain <siddhantjain027@gmail.com>
2024-08-12 20:49:50 +0530

    integrated mongodb and prisma

---
+++ actions/create-board.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+
+const CreateBoard = z.object({
+    title: z.string(),
+});
+
+
+export async function create(formData: FormData){
+    const { title } = CreateBoard.parse({
+        title: formData.get("title"),
+    });
+    await db.board.create({
+      data: {
+        title,
+      }
+    });
+
+    revalidatePath("/organization/org_2jsu4S3Oi4m8Jol4vLGZBuHjXxV");
+  }
--- app/(platform)/(dashboard)/organization/[organisationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organisationId]/page.tsx
@@ -1,15 +1,52 @@
-import { OrganizationSwitcher } from '@clerk/nextjs';
-import { auth } from '@clerk/nextjs/server';
-import React from 'react'
+// "use client";
+// import { OrganizationSwitcher } from '@clerk/nextjs';
+// import { auth } from '@clerk/nextjs/server';
+// import React from 'react'
 
-const organizationPage = () => {
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+
+
+
+
+const organizationPage = async () => {
     //to access any auth related information this is how you use this 
-    const { userId, orgId} = auth();
+    // const { userId, orgId} = auth();
+    // async function create(formData: FormData){
+    //   "use server";
+    //   const title = formData.get("title") as string;
+    //   await db.board.create({
+    //     data: {
+    //       title,
+    //     }
+    //   });
+    // }
+  const boards = await db.board.findMany();
   return (
-    <div>
+    <div className="flex flex-col space-y-4 ">
       {/* <OrganizationSwitcher 
         hidePersonal/> */}
-        Orgaisation page
+       <form action={create}>
+          <input 
+            id="title"
+            name="title"
+            required
+            placeholder="Enter a board title"
+            className="border-black border p-1"
+          />
+
+          <Button type="submit">
+            Submit
+          </Button>
+       </form>
+       <div className="space-y-2">
+         {boards.map((board) => (
+          <div key={board.id}>
+            Board title: {board.title}
+          </div>
+         ))}
+       </div>
     </div>
   )
 }
