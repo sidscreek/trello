982632e713f9a883a56193f45d20583fcb01d1b6
SiddhantJain <siddhantjain027@gmail.com>
2024-08-14 12:46:47 +0530

    added skeleton to the project board

--- app/(platform)/(dashboard)/organization/[organisationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organisationId]/page.tsx
@@ -3,40 +3,14 @@
 // import { auth } from '@clerk/nextjs/server';
 // import React from 'react'
 
-import { create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
-import { db } from "@/lib/db";
-import { Board } from "./board";
-import { Form } from "./form";
-
-
-
+import { Info } from "./_components/Info";
 
 const organizationPage = async () => {
-    //to access any auth related information this is how you use this 
-    // const { userId, orgId} = auth();
-    // async function create(formData: FormData){
-    //   "use server";
-    //   const title = formData.get("title") as string;
-    //   await db.board.create({
-    //     data: {
-    //       title,
-    //     }
-    //   });
-    // }
-  const boards = await db.board.findMany();
   return (
-    <div className="flex flex-col space-y-4 ">
-      {/* <OrganizationSwitcher 
-        hidePersonal/> */}
-       <Form />
-       <div className="space-y-2">
-         {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id}/>
-         ))}
-       </div>
+    <div className="w-full mb-20">
+      <Info />
     </div>
-  )
-}
+  );
+};
 
 export default organizationPage
